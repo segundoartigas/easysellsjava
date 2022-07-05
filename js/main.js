0bc9addf7a4c3509aeff7e8d84b6f0264c6daf83
segundoartigas <segundoartigas2001@gmail.com>
2022-07-04 22:03:46 -0300

    desafio2

--- js/main.js
+++ js/main.js
@@ -19,4 +19,38 @@ else if (dineroNombre >= 9000 && dineroNombre <11000) {
 else {
     alert("genial puedes comprar lo que desees")
 }
+let cel = prompt("te daremos informacion acerca de nuestros celulares, reponde ok para ver la info.");
 
+class Celular{
+    constructor(color,peso,modelo,ram,rdc){
+        this.color = color;
+        this.peso = peso;
+        this.modelo = modelo;
+        this.ram = ram;
+        this.rdc = rdc;
+        this.encendido = false;
+    }
+    PresionarBotonEncendido(){
+        if(this.encendido == false){
+            alert("el celular se puede prender")
+            this.encendido = true
+        } else{
+            alert("el celular se puede apagar tambien")
+            this.encendido = false
+        }
+    }
+    Reiniciar(){
+        if (this.encendido == true){
+            alert("el celular se reinicia")
+        } else{
+            alert("el celular tambien se apaga bien")
+        }
+    }
+    grabarVideo(){
+        alert(`el celular graba fotos y toma videos con la gran resolucion de camara`)
+    }
+}
+celular1 = new Celular("azul","5 pulgadas","iphone 12","64 gb","100px");
+celular1.PresionarBotonEncendido();
+celular1.Reiniciar();
+celular1.grabarVideo();
