cbea9a2cdfc3a0b7a8701db3a54f86f71c42fad4
segundoartigas <segundoartigas2001@gmail.com>
2022-07-25 19:27:12 -0300

    correcciones y agregando librerias

--- js/main.js
+++ js/main.js
@@ -1,78 +1,23 @@
-let saludar = prompt("Bienvenido a esasysells que disfrutes del recorrido en nuestra pagina")
-let nombre = prompt("dime tu nombre");
-alert("hola " + nombre);
-let number = prompt("escribi tu edad");
-alert("tienes " + number);
-dineroNombre = prompt("cuanto dinero tienes para gastar?");
-if (dineroNombre >= 1000 && dineroNombre < 3000) {
-    alert("comprate un iphone")
-}
-else if (dineroNombre >= 0 && dineroNombre <999) {
-    alert("Lo siento no puedes comprar nada")
-}
-else if (dineroNombre >= 5000 && dineroNombre <6999) {
-    alert("comprate una heladera")
-}
-else if (dineroNombre >= 9000 && dineroNombre <11000) {
-    alert("comprate un auto")
-}
-else {
-    alert("genial puedes comprar lo que desees")
-}
-let cel = prompt("te daremos informacion acerca de nuestros celulares, reponde ok para ver la info.");
-// Clase para tirar alerts de lo que hace el celular.
-class Celular{
-    constructor(color,peso,modelo,ram,rdc){
-        this.color = color;
-        this.peso = peso;
-        this.modelo = modelo;
-        this.ram = ram;
-        this.rdc = rdc;
-        this.encendido = false;
-    }
-    PresionarBotonEncendido(){
-        if(this.encendido == false){
-            alert("el celular se puede prender")
-            this.encendido = true
-        } else{
-            alert("el celular se puede apagar tambien")
-            this.encendido = false
-        }
-    }
-    Reiniciar(){
-        if (this.encendido == true){
-            alert("el celular se reinicia")
-        } else{
-            alert("el celular tambien se apaga bien")
-        }
-    }
-    grabarVideo(){
-        alert(`el celular graba fotos y toma videos con la gran resolucion de camara`)
-    }
-}
-celular1 = new Celular("azul","5 pulgadas","iphone 12","64 gb","100px");
-celular1.PresionarBotonEncendido();
-celular1.Reiniciar();
-celular1.grabarVideo();
-
+let saludar = swal("Bienvenido a esasysells que disfrutes del recorrido en nuestra pagina")
 //Arrancan los eventos, sirven en el comprar de las tarjetas.
 let button = document.getElementById("compra");
 button.addEventListener("click", respuestaClick);
 function respuestaClick() {
     console.log("Primer respuesta");
-    alert("Seleccionaste la compra del iphone 12 pro max.")
+    swal("Perfecto, seleccionaste el iphone!!", "hecho");
 
-let button2 = document.getElementById("heladera");
+
+let button2 = document.getElementById("compra");
 button2.addEventListener("click", respuestaClick);
 function respuestaClick() {
     console.log("Segunda respuesta");
-    alert("Seleccionaste la compra de la heladera patrick.")
+    swal("Perfecto, seleccionaste la heladera!!", "hecho");
 
-let button3 = document.getElementById("bmw");
+let button3 = document.getElementById("compra");
 button3.addEventListener("click", respuestaClick);
 function respuestaClick() {
     console.log("Tercer respuesta");
-    alert("Seleccionaste la compra del auto bmw m3 0km.")
+    swal("Perfecto, seleccionaste el bmw!!", "hecho");
 }
 }
 }
@@ -95,32 +40,149 @@ console.log(navbar.innerHTML);
 console.log(main.innerHTML);
 console.log(oferta.innerHTML);
 
-//Utilizando local storage y guardando datos.
-localStorage.setItem("Bienvenidos", "En easysells, nos contactamos con vos para hacer de tu compra o venta un experiencia simple y rapida.");
+// Variables
+const baseDeDatos = [
+    {
+        id: 1,
+        nombre: 'iphone 12 pro max',
+        precio: 120.000,
+        imagen: './img/iphone-12.png'
+    },
+    {
+        id: 2,
+        nombre: 'bmw negro',
+        precio: 5.000,
+        imagen: './img/bmw.jpg'
+    },
+    {
+        id: 3,
+        nombre: 'heladera',
+        precio: 40.000,
+        imagen: './img/hel.png'
+    },
+    {
+        id: 4,
+        nombre: 'notebook hp',
+        precio: 130.000,
+        imagen: './img/notebook.jpg'
+    }
 
-let mensaje_local = localStorage.getItem("Bienvenidos");
-console.log(mensaje_local);
-alert("el valor que guarde es: " +localStorage.getItem("esValido"))
+];
 
-//session storage: guardar datos
-sessionStorage.setItem("Acerca de nosotros", "En easysells, nos contactamos con vos para hacer de tu compra o venta un experiencia simple y rapida.");
+let carrito = [];
+const divisa = '$';
+const DOMitems = document.querySelector('#items');
+const DOMcarrito = document.querySelector('#carrito');
+const DOMtotal = document.querySelector('#total');
+const DOMbotonVaciar = document.querySelector('#boton-vaciar');
 
-//recuperando datos
-let mensaje_session = sessionStorage.getItem("Acerca de nosotros");
-console.log(mensaje_session);
+// Funciones
+function renderizarProductos() {
+    baseDeDatos.forEach((info) => {
+        // Estructura
+        const miNodo = document.createElement('div');
+        miNodo.classList.add('card', 'col-sm-4');
+        // Body
+        const miNodoCardBody = document.createElement('div');
+        miNodoCardBody.classList.add('card-body');
+        // Titulo
+        const miNodoTitle = document.createElement('h5');
+        miNodoTitle.classList.add('card-title');
+        miNodoTitle.textContent = info.nombre;
+        // Imagen
+        const miNodoImagen = document.createElement('img');
+        miNodoImagen.classList.add('img-fluid');
+        miNodoImagen.setAttribute('src', info.imagen);
+        // Precio
+        const miNodoPrecio = document.createElement('p');
+        miNodoPrecio.classList.add('card-text');
+        miNodoPrecio.textContent = `${info.precio}${divisa}`;
+        // Boton 
+        const miNodoBoton = document.createElement('button');
+        miNodoBoton.classList.add('btn', 'btn-primary');
+        miNodoBoton.textContent = '+';
+        miNodoBoton.setAttribute('marcador', info.id);
+        miNodoBoton.addEventListener('click', anyadirProductoAlCarrito);
+        // Insertamos
+        miNodoCardBody.appendChild(miNodoImagen);
+        miNodoCardBody.appendChild(miNodoTitle);
+        miNodoCardBody.appendChild(miNodoPrecio);
+        miNodoCardBody.appendChild(miNodoBoton);
+        miNodo.appendChild(miNodoCardBody);
+        DOMitems.appendChild(miNodo);
+    });
+}
 
-//json
-const primerProducto = {id:2, producto:"computadora"};
-localStorage.setItem("primerProducto", primerProducto);
-let producto_local = localStorage.getItem("primerProducto");
-console.log(producto_local.producto);
+/**
+ * Evento para añadir un producto al carrito de la compra
+ */
+function anyadirProductoAlCarrito(evento) {
+    carrito.push(evento.target.getAttribute('marcador'))
+    renderizarCarrito();
 
-const enJson = JSON.stringify(primerProducto);
-console.log(enJson);
-console.log(typeof primerProducto);
-console.log(typeof enJson);
+}
 
-localStorage.setItem("primerProducto", enJson);
-localStorage.setItem("primerProducto", JSON.stringify(primerProducto));
+function renderizarCarrito() {
+    DOMcarrito.textContent = '';
+    const carritoSinDuplicados = [...new Set(carrito)];
+    // Generamos los Nodos a partir de carrito
+    carritoSinDuplicados.forEach((item) => {
+        const miItem = baseDeDatos.filter((itemBaseDatos) => {
+            return itemBaseDatos.id === parseInt(item);
+        });
+        const numeroUnidadesItem = carrito.reduce((total, itemId) => {
+            return itemId === item ? total += 1 : total;
+        }, 0);
+        // Creamos el nodo del item del carrito
+        const miNodo = document.createElement('li');
+        miNodo.classList.add('list-group-item', 'text-right', 'mx-2');
+        miNodo.textContent = `${numeroUnidadesItem} x ${miItem[0].nombre} - ${miItem[0].precio}${divisa}`;
+        // Boton de borrar
+        const miBoton = document.createElement('button');
+        miBoton.classList.add('btn', 'btn-danger', 'mx-5');
+        miBoton.textContent = 'X';
+        miBoton.style.marginLeft = '1rem';
+        miBoton.dataset.item = item;
+        miBoton.addEventListener('click', borrarItemCarrito);
+        miNodo.appendChild(miBoton);
+        DOMcarrito.appendChild(miNodo);
+    });
+    DOMtotal.textContent = calcularTotal();
+}
 
+/**
+ * Evento para borrar un elemento del carrito
+ */
+function borrarItemCarrito(evento) {
+    const id = evento.target.dataset.item;
+    carrito = carrito.filter((carritoId) => {
+        return carritoId !== id;
+    });
+    renderizarCarrito();
+}
+
+/**
+ * Calcula el precio total teniendo en cuenta los productos repetidos
+ */
+function calcularTotal() { 
+    return carrito.reduce((total, item) => {
+        const miItem = baseDeDatos.filter((itemBaseDatos) => {
+            return itemBaseDatos.id === parseInt(item);
+        });
+        return total + miItem[0].precio;
+    }, 0).toFixed(2);
+}
+
+/**
+ * Varia el carrito y vuelve a dibujarlo
+ */
+function vaciarCarrito() {
+    carrito = [];
+    renderizarCarrito();
+}
+
+DOMbotonVaciar.addEventListener('click', vaciarCarrito);
+
+renderizarProductos();
+renderizarCarrito();
 
