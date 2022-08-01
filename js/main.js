fba671bcfe3389c48802aa8e12876931bd50df77
segundoartigas <segundoartigas2001@gmail.com>
2022-08-01 20:51:30 -0300

    agregando fetch

--- js/main.js
+++ js/main.js
@@ -186,3 +186,30 @@ DOMbotonVaciar.addEventListener('click', vaciarCarrito);
 renderizarProductos();
 renderizarCarrito();
 
+//utilizando fetch
+fetch("https://reqres.in/api/users",{
+    method : "POST",
+    body : '{"name" : "easysells", "empresa" : "ventas y compras"}',
+    headers: {"content-type" : "application/json"}
+})
+.then(res=>res.json())
+.then(res=>console.log(res))
+
+//productos json
+fetch('js/productos.json')
+.then((Response) => Response.json())
+.then((data) => {
+    let div_padre = document.createElement("div");
+    div_padre.className = "card";
+    let div_hijo1 = document.createElement("div");
+    div_hijo1.className = "card-header";
+    let div_hijo2 = document.createElement("div");
+    div_hijo2.className = "card-body";
+    let parrafo = document.createElement("p");
+    div_hijo1.innerHTML = '${valor.nombre} <b>($${valor.precio})</b>';
+    parrafo.innerText = valor.descripcion;
+    div_hijo2.appendChild(parrafo);
+    div_padre.appendChild(div_hijo1);
+    div_padre.appendChild(div_hijo2);
+    resultado.appendChild(div_padre);
+})
