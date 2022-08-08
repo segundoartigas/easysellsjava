aed0000f0445879e34fd281802206a508b1804f9
segundoartigas <segundoartigas2001@gmail.com>
2022-08-08 17:45:40 -0300

    fetch cambios

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
 let saludar = swal("Bienvenido a esasysells que disfrutes del recorrido en nuestra pagina")
 //Arrancan los eventos, sirven en el comprar de las tarjetas.
 let button = document.getElementById("compra");
-button.addEventListener("click", respuestaClick);
+    
+
 function respuestaClick() {
     console.log("Primer respuesta");
     swal("Perfecto, seleccionaste el iphone!!", "hecho");
@@ -26,7 +27,6 @@ let barra = document.getElementsByClassName("barra");
 console.log(barra[0].innerHTML);
 console.log(barra[1].innerHTML);
 console.log(barra[2].innerHTML);
-console.log(barra[3].innerHTML);
 
 
 let body = document.getElementById("grid-container");
@@ -37,8 +37,6 @@ let oferta = document.getElementById("oferta");
 console.log(body.innerHTML);
 console.log(header.innerHTML);
 console.log(navbar.innerHTML);
-console.log(main.innerHTML);
-console.log(oferta.innerHTML);
 
 // Variables
 const baseDeDatos = [
@@ -65,6 +63,36 @@ const baseDeDatos = [
         nombre: 'notebook hp',
         precio: 130.000,
         imagen: './img/notebook.jpg'
+    },
+    {
+        id: 5,
+        nombre: 'Bicicleta rodado 30',
+        precio: 100.000,
+        imagen: './img/bici.jpg'
+    },
+    {
+        id: 6,
+        nombre: 'Guitarra',
+        precio: 35.000,
+        imagen: './img/guitarra.jpg'
+    },
+    {
+        id: 7,
+        nombre: 'Horno',
+        precio: 70.000,
+        imagen: './img/horno.jpg'
+    },
+    {
+        id: 8,
+        nombre: 'Moto bmw',
+        precio: 335.000,
+        imagen: './img/moto.jpg'
+    },
+    {
+        id: 9,
+        nombre: 'Aire acondicionado',
+        precio: 24.000,
+        imagen: './img/aire.jpg'
     }
 
 ];
@@ -186,68 +214,15 @@ DOMbotonVaciar.addEventListener('click', vaciarCarrito);
 renderizarProductos();
 renderizarCarrito();
 
-//utilizando fetch
-fetch("https://reqres.in/api/users",{
-    method : "POST",
-    body : '{"name" : "easysells", "empresa" : "ventas y compras"}',
-    headers: {"content-type" : "application/json"}
-})
-.then(res=>res.json())
-.then(res=>console.log(res))
+// //utilizando fetch
+// fetch("https://reqres.in/api/users",{
+//     method : "POST",
+//     body : '{"name" : "easysells", "empresa" : "ventas y compras"}',
+//     headers: {"content-type" : "application/json"}
+// })
+// .then(res=>res.json())
+// .then(res=>console.log(res))
 
-//productos json
-// fetch('./productos.json')
-// .then((response) => response.json())
-// .then((data) => {
-//     const resultado = document.getElementById("resultado");
-//     data.forEach(valor =>{
-//         let div_padre = document.createElement("div");
-//         div_padre.className = "card";
-//         let div_hijo1 = document.createElement("div");
-//         div_hijo1.className = "card-header";
-//         let div_hijo2 = document.createElement("div");
-//         div_hijo2.className = `card-body`;
-//         let parrafo = document.createElement("p");
-//         div_hijo1.innerHTML = `${valor.nombre} <b>($${valor.precio})</b>`;
-//         parrafo.innerText = valor.descripcion;
-//         div_hijo2.appendChild(parrafo);
-//         document.body.append(div_padre);
-//         div_padre.appendChild(div_hijo2);
-//         resultado.appendChild(div_padre);
-//     })
-    
-// }).catch(error => console.log("fallo!"));
+// //productos json
 
 
-async function obtenerJSON() {
-    const URLJSON = "/productos.json";
-    const respuesta = await fetch(URLJSON);
-    const data = await respuesta.json();
-    return respuesta;
-}
-
-obtenerJSON().then(data => {
-    const resultado = document.getElementById("resultado");
-    data.forEach(valor =>{
-        console.log("GANGSTERRRR", valor)
-        let div_padre = document.createElement("div");
-        div_padre.className = "card";
-        let div_hijo1 = document.createElement("div");
-        div_hijo1.className = "card-header";
-        let div_hijo2 = document.createElement("div");
-        div_hijo2.className = `card-body`;
-        let parrafo = document.createElement("p");
-        div_hijo1.innerHTML = `${valor.nombre} <b>($${valor.precio})</b>`;
-        parrafo.innerText = valor.descripcion;
-        div_hijo2.appendChild(parrafo);
-        document.body.append(div_padre);
-        div_padre.appendChild(div_hijo2);
-        resultado.appendChild(div_padre);
-    })
-}).catch(error => console.log("fallo!"));
-// const getJSON = async () => {
-//     json = await obtenerJSON()
-//     console.log("gangster", json)
-// }
-
-// getJSON();
---
+++ js/tienda.js
@@ -0,0 +1,29 @@
+Headers = new File('Access-Control-Allow-Origin: *');
+fetch('js/tienda.json')
+.then((response) => response.json())
+.then((data) => {
+    const api = document.getElementById("api");
+    data.forEach(visual => {
+        let titulo = document.createElement("p");
+        
+
+        let div1 = document.createElement("div");
+        div1.classList = "card w-25 d-flex row";
+        
+        let div2 = document.createElement("div");
+        div2.classList = "card-header m-2";
+        
+        let img = document.createElement("img");
+        
+        titulo.innerText = `${visual.descripción}`;
+        div2.innerHTML = `${visual.nombre}`;
+        img.src = "img/" + visual.imagen;
+        img.width = 100;
+        img.length = 150;
+
+        div1.appendChild(titulo);
+        div1.appendChild(div2);
+        div2.appendChild(img);
+        api.appendChild(div1);
+    });
+});
